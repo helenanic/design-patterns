67fa30ac131f4791f6304f390104433bdd577518
helenanic <helenanicolodelli@gmail.com>
2018-04-16 22:52:03 -0300

    adding singleton

---
+++ design-patterns/src/br/com/patterns/singleton/Singleton.java
@@ -0,0 +1,17 @@
+package br.com.patterns.singleton;
+
+public class Singleton {
+
+	static private  Singleton instance;
+	
+	private Singleton() {
+		
+	}
+	
+	public static Singleton getInstance() {
+		if(instance == null) {
+			instance = new Singleton();
+		}
+		return instance;
+	}
+}
